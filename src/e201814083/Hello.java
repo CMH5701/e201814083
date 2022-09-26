da440bfc292f876e2d8ae2f8983285120d4d52d5
CMH5701 <skdaks5701@naver.com>
2022-09-26 16:39:43 +0900

    최초 커밋

---
+++ src/e201814083/Hello.java
@@ -0,0 +1,7 @@
+package e201814083;
+
+public class Hello {
+	public void say() {
+System.out.println("hello");
+}
+	}
---
+++ src/e201814083/Main.java
@@ -0,0 +1,6 @@
+package e201814083;
+
+public class Main { public static void main(String[] args) {
+	System.out.println("hello world");
+	}
+}
